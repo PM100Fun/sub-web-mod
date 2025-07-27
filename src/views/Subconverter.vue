061005f5e9410270176b2bc49b0ebd5e13778fa3
PM250 <rexriviera@qq.com>
2025-07-27 21:31:19 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -499,6 +499,10 @@ export default {
             label: "通用",
             options: [
               {
+                label: "Basic",
+                value: "https://raw.githubusercontent.com/SleepyHeeead/subconverter-config/master/remote-config/special/basic.ini"
+              }
+              {
                 label: "默认大咕咕鸡",
                 value: "https://raw.githubusercontent.com/pm100fun/subconverter-mod/master/base/config/example_external_config.ini"
               },
